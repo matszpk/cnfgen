209d27e569e09b8fc41ac3f2ec8c4e0e261670e2
matszpk <matszpk@interia.pl>
2025-02-14 08:39:58 +0100

    Change license to less restrictive.

--- src/intexpr/bin_arith.rs
+++ src/intexpr/bin_arith.rs
@@ -1,21 +1,4 @@
 // bin_arith.rs - integer expression structures.
-//
-// cnfgen - Generate the DIMACS CNF formula from operations
-// Copyright (C) 2022  Mateusz Szpakowski
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 //! The module contains binary operators definitions.
